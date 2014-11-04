b1883f11a5766b33b18d83795f27c7906e053525
mongrolwarrior <mongrolwarrior@gmail.com>
2014-11-04 22:17:21 +1000

    Initial Commit

---
+++ EPGC 01Tests/EPGC_01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  EPGC_01Tests.swift
+//  EPGC 01Tests
+//
+//  Created by Andrew Amos on 4/11/2014.
+//  Copyright (c) 2014 Andrew Amos. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class EPGC_01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
